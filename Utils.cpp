de90b21dc30fecb53b70d914def5bbb7205326c8
emilianotca <emilianotca@protonmail.ch>
2021-08-13 19:32:06 -0300

    Implementing 'Mergesort'. Implementing 'Radixsort'. Removing the 'name' parameter because it's not needed.

--- Utils.cpp
+++ Utils.cpp
@@ -4,7 +4,7 @@
 
 #include "Utils.h"
 
-void Particao(Person* A, int Esq, int Dir, int* i, int* j, bool name)
+void Particao(Person* A, int Esq, int Dir, int* i, int* j)
 {
     Person x, w;
     *i = Esq;
@@ -12,70 +12,51 @@ void Particao(Person* A, int Esq, int Dir, int* i, int* j, bool name)
 
     x = A[(*i + *j)/2]; /* Obtém o pivô x */
 
-    if(name) // Considerar o campo 'nome' do tipo 'Person' para ordenação
-    {
-        do {
-            while(A[*i].get_name() < x.get_name())
-            {
-                (*i)++;
-            }
-            while(A[*j].get_name() > x.get_name())
-            {
-                (*j)--;
-            }
+    do {
+        while(A[*i].get_name() < x.get_name())
+        {
+            (*i)++;
+        }
+        while(A[*j].get_name() > x.get_name())
+        {
+            (*j)--;
+        }
 
-            if(*i <= *j)
-            {
-                w = A[*i];
-                A[*i] = A[*j];
-                A[*j] = w;
-                (*i)++;
-                (*j)--;
-            }
-        }while(*i <= *j); /* Partição */
-    }
-    else // Considerar o campo 'conscience' do tipo 'Person' para ordenação
-    {
-        do {
-            while(A[*i].get_conscience() < x.get_conscience()) {(*i)++;}
-            while(A[*j].get_conscience() > x.get_conscience()) {(*j)--;}
-
-            if(*i <= *j)
-            {
-                w = A[*i];
-                A[*i] = A[*j];
-                A[*j] = w;
-                (*i)++;
-                (*j)--;
-            }
-        }while(*i <= *j); /* Partição */
-    }
+        if(*i <= *j)
+        {
+            w = A[*i];
+            A[*i] = A[*j];
+            A[*j] = w;
+            (*i)++;
+            (*j)--;
+        }
+    }while(*i <= *j); /* Partição */
 }
 
-void Ordena(Person *A, int Esq, int Dir, bool name)
+void Ordena(Person *A, int Esq, int Dir)
 {
     int i, j;
 
-    Particao(A, Esq, Dir, &i, &j, name);
+    Particao(A, Esq, Dir, &i, &j);
 
     if(Esq < j)
     {
-        Ordena(A, Esq, j, name);
+        Ordena(A, Esq, j);
     } /* Ordena */
     if(i < Dir)
     {
-        Ordena(A, i, Dir, name);
+        Ordena(A, i, Dir);
     } /* Ordena */
 }
 
-void Quicksort(Person* A,int vector_size, bool name)
+void Quicksort(Person* A,int vector_size)
 {
     int temp = vector_size - 1;
 
-    Ordena(A, 0, temp, name);
+    Ordena(A, 0, temp);
 }
 
-void Refaz(int Esq, int Dir, Person* A, bool name)
+void Refaz(int Esq, int Dir, Person* A)
 {
     int i, j;
     Person x;
@@ -84,53 +65,29 @@ void Refaz(int Esq, int Dir, Person* A, bool name)
     j = i * 2;
     x = A[i];
 
-    if(name) // Considera o campo 'name' do tipo 'Person' para a ordenação
+    while(j <= Dir)
     {
-        while(j <= Dir)
+        if(j < Dir)
         {
-            if(j < Dir)
+            if(A[j].get_conscience() < A[j+1].get_conscience())
             {
-                if(A[j].get_name() < A[j+1].get_name())
-                {
-                    j++;
-                }
+                j++;
             }
-            if(x.get_name() >= A[j].get_name())
-            {
-                break;
-            }
-
-            A[i] = A[j];
-            i = j;
-            j = i * 2;
         }
-        A[i] = x;
-    }
-    else // Considera o campo 'conscience' do tipo 'Person' para a ordenação
-    {
-        while(j <= Dir)
+        if(x.get_conscience() >= A[j].get_conscience())
         {
-            if(j < Dir)
-            {
-                if(A[j].get_conscience() < A[j+1].get_conscience())
-                {
-                    j++;
-                }
-            }
-            if(x.get_conscience() >= A[j].get_conscience())
-            {
-                break;
-            }
-
-            A[i] = A[j];
-            i = j;
-            j = i * 2;
+            break;
         }
-        A[i] = x;
+
+        A[i] = A[j];
+        i = j;
+        j = i * 2;
     }
+
+    A[i] = x;
 }
 
-void Constroi(Person* A, int n, bool name)
+void Constroi(Person* A, int n)
 {
     int Esq;
 
@@ -139,17 +96,17 @@ void Constroi(Person* A, int n, bool name)
     while(Esq > 0)
     {
         Esq--;
-        Refaz(Esq, n, A, name);
+        Refaz(Esq, n, A);
     }
 }
 
-void Heapsort(Person* A, int n, bool name)
+void Heapsort(Person* A, int n)
 {
     int Esq, Dir;
     int temp = n-1;
     Person x;
 
-    Constroi(A, temp, name);
+    Constroi(A, temp);
 
     Esq = 0;
     Dir = temp;
@@ -162,6 +119,114 @@ void Heapsort(Person* A, int n, bool name)
 
         Dir--;
 
-        Refaz(Esq, Dir, A, name);
+        Refaz(Esq, Dir, A);
     }
+}
+
+void merge(Person* array, int const left, int const mid, int const right)
+{
+    auto const subArrayOne = mid - left + 1;
+    auto const subArrayTwo = right - mid;
+    auto *leftArray = new Person[subArrayOne],
+    *rightArray = new Person[subArrayTwo];
+
+    for (auto i = 0; i < subArrayOne; i++)
+        leftArray[i] = array[left + i];
+    for (auto j = 0; j < subArrayTwo; j++)
+        rightArray[j] = array[mid + 1 + j];
+
+    auto indexOfSubArrayOne = 0,
+    indexOfSubArrayTwo = 0;
+    int indexOfMergedArray = left;
+
+    while (indexOfSubArrayOne < subArrayOne && indexOfSubArrayTwo < subArrayTwo) {
+        if (leftArray[indexOfSubArrayOne].get_name() <= rightArray[indexOfSubArrayTwo].get_name()) {
+            array[indexOfMergedArray] = leftArray[indexOfSubArrayOne];
+            indexOfSubArrayOne++;
+        }
+        else {
+            array[indexOfMergedArray] = rightArray[indexOfSubArrayTwo];
+            indexOfSubArrayTwo++;
+        }
+        indexOfMergedArray++;
+    }
+
+    while (indexOfSubArrayOne < subArrayOne) {
+        array[indexOfMergedArray] = leftArray[indexOfSubArrayOne];
+        indexOfSubArrayOne++;
+        indexOfMergedArray++;
+    }
+
+    while (indexOfSubArrayTwo < subArrayTwo) {
+        array[indexOfMergedArray] = rightArray[indexOfSubArrayTwo];
+        indexOfSubArrayTwo++;
+        indexOfMergedArray++;
+    }
+}
+
+void mergeSort(Person* array, int const begin, int const end)
+{
+    if (begin >= end)
+        return;
+
+    auto mid = begin + (end - begin) / 2;
+
+    mergeSort(array, begin, mid);
+    mergeSort(array, mid + 1, end);
+    merge(array, begin, mid, end);
+}
+
+void exch(Person* A, int subj0, int subj1)
+{
+    Person temp = A[subj0];
+
+    // Swapping subj and subj1
+    A[subj0] = A[subj1];
+    A[subj1] = temp;
+}
+
+char digit(const Person& A, int position)
+{
+    return A.get_conscience().at(position);
+}
+
+void quicksortB(Person* A, int l, int r, int w)
+{
+    int i {l};
+    int j {r};
+
+    if(r <= l)
+    {
+        return;
+    }
+
+    while(j != i)
+    {
+        while(digit(A[i], w) == '0' && (i < j))
+        {
+            i++;
+        }
+
+        while(digit(A[j], w) == '1' && (j > i))
+        {
+            j--;
+        }
+
+        exch(A, i, j);
+    }
+
+    if(digit(A[r], w) == 0)
+    {
+        j++;
+    }
+
+    quicksortB(A, l, j-1, w-1);
+    quicksortB(A, j, r, w-1);
+}
+
+void Radixsort(Person* A, int l, int r)
+{
+    int con_length = static_cast<int>(A->get_conscience().length()) - 1;
+
+    quicksortB(A, l, r-1, con_length);
 }
--- Utils.h
+++ Utils.h
@@ -3,15 +3,22 @@
 //
 
 /**
- * O algoritmo do Quicksort foi reproduzido da fonte 0, ao passo que os outros foram reproduzidos da fonte 1; ambas
- * serão citadas abaixo separadamente. O autor deste trabalho prático não detém autoria sobre nenhum dos algoritmos de
- * ordenação nele utilizados, exceto pela adaptação para a linguagem C++11 quando necessária e os detalhes de
- * implementaçao pertinentes apenas à este trabalho.
+ * Os algoritmos de ordenação implementados aqui foram obtidos das fontes identificadas ao fim desta seção de
+ * comentários. O autor deste trabalho prático não detém autoria sobre nenhum dos algoritmos de ordenação nele
+ * utilizados, exceto pela adaptação para a linguagem C++11 quando necessária e os detalhes de implementaçao pertinentes
+ * apenas à este trabalho.
+ * Quicksort: Fonte 0
+ * Heapsort: Fonte 1
+ * Mergesort e Radixsort: Fonte 2
+ *
  * Fonte 0: Projeto de Algoritmos com Implementações em Pascal e C
- *        Nívio Ziviani, PhD
- *        Editora Pioneira, 4a Edição
+ *          Nívio Ziviani, PhD
+ *          Editora Pioneira, 4a Edição
  * Fonte 1: Slides da disciplina DCC205: Estruturas De Dados, da Universidade Federal de Minas Gerais
  *          Professores: Luiz Chaimowicz e Raquel Prates
+ * Fonte 2: Website Geeks for GeekS
+ *          Acesso em 12-08-2021
+ *          Endereço eletrônico: https://www.geeksforgeeks.org/radix-sort/ e https://www.geeksforgeeks.org/merge-sort/
  */
 
 #ifndef ORDENACAO_INTERPLANETARIA_UTILS_H
@@ -20,16 +27,15 @@
 #include "Person.h"
 
 /**
- * Função de partiçao para o Quicksort
+ * Função de partição para o Quicksort
  *
  * @param A Vetor de pessoas a ser ordenado
  * @param Esq Índice da esquerda do vetor
  * @param Dir Índice da direita do vetor
  * @param i Auxiliar para ordenação
  * @param j Auxiliar para ordenação
- * @param name Parâmetro booleano para definir alvo da ordenação: se falso, ordena com base na consciência
  */
-void Particao(Person* A, int Esq, int Dir, int* i, int* j, bool name = false);
+void Particao(Person* A, int Esq, int Dir, int* i, int* j);
 
 /**
  * Funçao de ordenação para o Quicksort
@@ -37,18 +43,16 @@ void Particao(Person* A, int Esq, int Dir, int* i, int* j, bool name = false);
  * @param A Vetor de pessoas a ser ordenado
  * @param Esq Índice da esquerda do vetor
  * @param Dir Índice da direita do vetor
- * @param name Parâmetro booleano para definir alvo da ordenação: se falso, ordena com base na consciência
  */
-void Ordena(Person *A, int Esq, int Dir, bool name = false);
+void Ordena(Person *A, int Esq, int Dir);
 
 /**
  * Quicksort
  *
  * @param A Vetor de pessoas a ser rearranjado para Heap
  * @param vec_size Tamanho do vetor
- * @param name Parâmetro booleano para definir alvo da ordenação: se falso, ordena com base na consciência
  */
-void Quicksort(Person* A,int vector_size, bool name = false);
+void Quicksort(Person* A,int vector_size);
 
 /**
  * Função 'Refaz' para o Heapsort
@@ -56,26 +60,48 @@ void Quicksort(Person* A,int vector_size, bool name = false);
  * @param Esq Índice da esquerda
  * @param Dir Índice da direita
  * @param A Vetor de pessoas a ser ordenado
- * @param name Parâmetro booleano para definir alvo da ordenação: se falso, ordena com base na consciência
  */
-void Refaz(int Esq, int Dir, Person* A, bool name = false);
+void Refaz(int Esq, int Dir, Person* A);
 
 /**
  * Função 'Constroi' para o Heapsort
  *
  * @param A Vetor de pessoas a ser convertido em Heap
  * @param n Tamanho do vetor
- * @param name Parâmetro booleano para definir alvo da ordenação: se falso, ordena com base na consciência
  */
-void Constroi(Person* A, int n, bool name = false);
+void Constroi(Person* A, int n);
 
 /**
  * Heapsort
  *
  * @param A Vetor de pessoas a ser ordenado
  * @param n Tamanho do vetor
- * @param name Parâmetro booleano para definir alvo da ordenação: se falso, ordena com base na consciência
  */
-void Heapsort(Person* A, int n, bool name = false);
+void Heapsort(Person* A, int n);
+
+/**
+ *
+ * @param array
+ * @param left
+ * @param mid
+ * @param right
+ */
+void merge(Person* array, int left, int mid, int right);
+
+/**
+ *
+ * @param array
+ * @param begin
+ * @param end
+ */
+void mergeSort(Person* array, int begin, int end);
+
+void exch(Person* A, int subj0, int subj1);
+
+void quicksortB(Person* A, int l, int r, int w);
+
+char digit(const Person& A, int position);
+
+void Radixsort(Person* A, int l, int r);
 
 #endif //ORDENACAO_INTERPLANETARIA_UTILS_H
